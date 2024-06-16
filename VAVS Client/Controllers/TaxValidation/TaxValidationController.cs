c926efac4871f04347c69053c45545b20f814ad2
HponeNaingTun <hponenaingtun@gmail.com>
2024-06-16 08:18:13 +0630

    modified ui

--- VAVS Client/Controllers/TaxValidation/TaxValidationController.cs
+++ VAVS Client/Controllers/TaxValidation/TaxValidationController.cs
@@ -1,4 +1,5 @@
 ﻿using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Wordprocessing;
 using Microsoft.AspNetCore.Mvc;
 using System.Data;
 using System.Drawing;
@@ -26,6 +27,57 @@ namespace VAVS_Client.Controllers.TaxValidationController
          
         }
 
+        public IActionResult ExportExcel(PagingList<TaxValidation> taxValidations)
+        {
+            using (XLWorkbook wb = new XLWorkbook())
+            {
+                var ws = wb.Worksheets.Add("Tax Validations Data Report");
+                var headerCell = ws.Cell(1, 1);
+                headerCell.Value = "ပြည်တွင်းအခွန်များဦးစီးငှာန";
+                headerCell.AsRange().AddToNamed("HeaderRange");
+                ws.Range(1, 1, 1, 13).Merge().Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                ws.Range(1, 1, 1, 13).Style.Font.Bold = true;
+                ws.Range(1, 1, 1, 13).Style.Font.FontSize = 16;
+
+                var secondHeaderCell = ws.Cell(2, 1);
+                secondHeaderCell.Value = "VAVS";
+                secondHeaderCell.AsRange().AddToNamed("HeaderRange");
+                ws.Range(2, 1, 2, 13).Merge().Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                ws.Range(2, 1, 2, 13).Style.Font.Bold = true;
+                ws.Range(2, 1, 2, 13).Style.Font.FontSize = 15;
+
+                ws.Cell(3, 1).Value = "ယာဥ်ပိုင်ရှင် အချက်အလက်";
+                ws.Range(3, 1, 3, 7).Merge().Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                ws.Range(3, 1, 3, 7).Style.Font.Bold = true;
+                ws.Range(3, 1, 3, 7).Style.Font.FontSize = 14;
+
+                ws.Cell(3, 8).Value = "ယာဥ်အချက်အလက်";
+                ws.Range(3, 8, 3, 11).Merge().Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                ws.Range(3, 8, 3, 11).Style.Font.Bold = true;
+                ws.Range(3, 8, 3, 11).Style.Font.FontSize = 14;
+
+                ws.Cell(3, 12).Value = "အခွန်ဆိုင်ရာ အချက်အလက်";
+                ws.Range(3, 12, 3, 19).Merge().Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                ws.Range(3, 12, 3, 19).Style.Font.Bold = true;
+                ws.Range(3, 12, 3, 19).Style.Font.FontSize = 14;
+
+                // Set background color for the header rows
+                ws.Range("A3:S3").Style.Fill.BackgroundColor = XLColor.DodgerBlue;
+
+                DataTable dt = _serviceFactory.CreateTaxValidationService().MakeVehicleDataExcelData(taxValidations, HttpContext);
+                ws.Cell(4, 1).InsertTable(dt);
+                ws.Range("A4:N4").Style.Fill.BackgroundColor = XLColor.DodgerBlue;
+                ws.Rows().AdjustToContents();
+                ws.Rows().Height = 20;
+                ws.Columns().AdjustToContents();
+                ws.Columns().Style.Font.FontSize = 14;
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    wb.SaveAs(stream);
+                    return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", MakeExcelFileName());
+                }
+            }
+        } 
         public IActionResult PendingList(int? pageNo)
         {
             SessionService sessionService = _serviceFactory.CreateSessionServiceService();
@@ -48,54 +100,7 @@ namespace VAVS_Client.Controllers.TaxValidationController
                 if (Request.Query["export"] == "excel")
                 {
                     bool ExportAll = Request.Query["ExportAll"] == "true";
-                    using (XLWorkbook wb = new XLWorkbook())
-                    {
-                        var ws = wb.Worksheets.Add("Tax Validations Data Report");
-                        var headerCell = ws.Cell(1, 1);
-                        headerCell.Value = "ပြည်တွင်းအခွန်များဦးစီးငှာန";
-                        headerCell.AsRange().AddToNamed("HeaderRange");
-                        ws.Range(1, 1, 1, 13).Merge().Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
-                        ws.Range(1, 1, 1, 13).Style.Font.Bold = true;
-                        ws.Range(1, 1, 1, 13).Style.Font.FontSize = 16;
-
-                        var secondHeaderCell = ws.Cell(2, 1);
-                        secondHeaderCell.Value = "VAVS";
-                        secondHeaderCell.AsRange().AddToNamed("HeaderRange");
-                        ws.Range(2, 1, 2, 13).Merge().Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
-                        ws.Range(2, 1, 2, 13).Style.Font.Bold = true;
-                        ws.Range(2, 1, 2, 13).Style.Font.FontSize = 15;
-
-                        ws.Cell(3, 1).Value = "ယာဥ်ပိုင်ရှင် အချက်အလက်";
-                        ws.Range(3, 1, 3, 7).Merge().Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
-                        ws.Range(3, 1, 3, 7).Style.Font.Bold = true;
-                        ws.Range(3, 1, 3, 7).Style.Font.FontSize = 14;
-
-                        ws.Cell(3, 8).Value = "ယာဥ်အချက်အလက်";
-                        ws.Range(3, 8, 3, 11).Merge().Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
-                        ws.Range(3, 8, 3, 11).Style.Font.Bold = true;
-                        ws.Range(3, 8, 3, 11).Style.Font.FontSize = 14;
-
-                        ws.Cell(3, 12).Value = "အခွန်ဆိုင်ရာ အချက်အလက်";
-                        ws.Range(3, 12, 3, 19).Merge().Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
-                        ws.Range(3, 12, 3, 19).Style.Font.Bold = true;
-                        ws.Range(3, 12, 3, 19).Style.Font.FontSize = 14;
-
-                        // Set background color for the header rows
-                        ws.Range("A3:S3").Style.Fill.BackgroundColor = XLColor.DodgerBlue;
-
-                        DataTable dt = _serviceFactory.CreateTaxValidationService().MakeVehicleDataExcelData(_serviceFactory.CreateTaxValidationService().GetTaxValidationPendigListForExcelPagin(HttpContext, pageNo, pageSize), HttpContext);
-                        ws.Cell(4, 1).InsertTable(dt);
-                        ws.Range("A4:N4").Style.Fill.BackgroundColor = XLColor.DodgerBlue;
-                        ws.Rows().AdjustToContents();
-                        ws.Rows().Height = 20;
-                        ws.Columns().AdjustToContents();
-                        ws.Columns().Style.Font.FontSize = 14;
-                        using (MemoryStream stream = new MemoryStream())
-                        {
-                            wb.SaveAs(stream);
-                            return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", MakeExcelFileName());
-                        }
-                    }
+                    return ExportExcel(_serviceFactory.CreateTaxValidationService().GetTaxValidationPendigListForExcelPagin(HttpContext, pageNo, pageSize));
                 }
                 return View(taxValidations);
             }
@@ -132,54 +137,7 @@ namespace VAVS_Client.Controllers.TaxValidationController
                 if (Request.Query["export"] == "excel")
                 {
                     bool ExportAll = Request.Query["ExportAll"] == "true";
-                    using (XLWorkbook wb = new XLWorkbook())
-                    {
-                        var ws = wb.Worksheets.Add("Tax Validations Data Report");
-                        var headerCell = ws.Cell(1, 1);
-                        headerCell.Value = "ပြည်တွင်းအခွန်များဦးစီးငှာန";
-                        headerCell.AsRange().AddToNamed("HeaderRange");
-                        ws.Range(1, 1, 1, 13).Merge().Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
-                        ws.Range(1, 1, 1, 13).Style.Font.Bold = true;
-                        ws.Range(1, 1, 1, 13).Style.Font.FontSize = 16;
-
-                        var secondHeaderCell = ws.Cell(2, 1);
-                        secondHeaderCell.Value = "VAVS";
-                        secondHeaderCell.AsRange().AddToNamed("HeaderRange");
-                        ws.Range(2, 1, 2, 13).Merge().Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
-                        ws.Range(2, 1, 2, 13).Style.Font.Bold = true;
-                        ws.Range(2, 1, 2, 13).Style.Font.FontSize = 15;
-
-                        ws.Cell(3, 1).Value = "ယာဥ်ပိုင်ရှင် အချက်အလက်";
-                        ws.Range(3, 1, 3, 7).Merge().Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
-                        ws.Range(3, 1, 3, 7).Style.Font.Bold = true;
-                        ws.Range(3, 1, 3, 7).Style.Font.FontSize = 14;
-
-                        ws.Cell(3, 8).Value = "ယာဥ်အချက်အလက်";
-                        ws.Range(3, 8, 3, 11).Merge().Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
-                        ws.Range(3, 8, 3, 11).Style.Font.Bold = true;
-                        ws.Range(3, 8, 3, 11).Style.Font.FontSize = 14;
-
-                        ws.Cell(3, 12).Value = "အခွန်ဆိုင်ရာ အချက်အလက်";
-                        ws.Range(3, 12, 3, 19).Merge().Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
-                        ws.Range(3, 12, 3, 19).Style.Font.Bold = true;
-                        ws.Range(3, 12, 3, 19).Style.Font.FontSize = 14;
-
-                        // Set background color for the header rows
-                        ws.Range("A3:S3").Style.Fill.BackgroundColor = XLColor.DodgerBlue;
-
-                        DataTable dt = _serviceFactory.CreateTaxValidationService().MakeVehicleDataExcelData(_serviceFactory.CreateTaxValidationService().GetTaxValidationApprevedListForExcelPagin(HttpContext, pageNo, pageSize), HttpContext);
-                        ws.Cell(4, 1).InsertTable(dt);
-                        ws.Range("A4:N4").Style.Fill.BackgroundColor = XLColor.DodgerBlue;
-                        ws.Rows().AdjustToContents();
-                        ws.Rows().Height = 20;
-                        ws.Columns().AdjustToContents();
-                        ws.Columns().Style.Font.FontSize = 14;
-                        using (MemoryStream stream = new MemoryStream())
-                        {
-                            wb.SaveAs(stream);
-                            return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", MakeExcelFileName());
-                        }
-                    }
+                    return ExportExcel(_serviceFactory.CreateTaxValidationService().GetTaxValidationApprevedListForExcelPagin(HttpContext, pageNo, pageSize));
                 }
                 return View(taxValidations);
             }
